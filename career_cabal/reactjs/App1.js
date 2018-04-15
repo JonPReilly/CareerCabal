599d0a405a203a4f48eac35008e6e70bac4fc767
Jon Reilly <jonreilly1994@gmail.com>
2018-04-14 21:28:49 -0400

    Integrate react-trello

--- career_cabal/reactjs/App1.js
+++ career_cabal/reactjs/App1.js
@@ -1,17 +1,29 @@
 import React from "react"
 import { render } from "react-dom"
+import Board from 'react-trello'
 
+const data = {
+  lanes: [
+    {
+      id: 'lane1',
+      title: 'Planned Tasks',
+      label: '2/2',
+      cards: [
+        {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
+	    {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
+      ]
+    },
+    {
+      id: 'lane2',
+      title: 'Completed',
+      label: '0/0',
+      cards: []
+    }
+  ]
+};
 class App1 extends React.Component {
   render() {
-    return (
-     <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <h1>Hello React</h1>
-          </div>
-        </div>
-      </div>
-    )
+    return  <Board data={data} />
   }
 }
 
